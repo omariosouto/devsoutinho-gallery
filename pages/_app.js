c3f5d2f6f4a9029ea692369fdc972700a6a69f67
Mario Souto <mario.souto@nubank.com.br>
2021-07-04 04:05:07 -0300

    fix tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import 'tailwindcss/tailwind.css'
+import '../theme/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   // mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './components/**/*.{js,ts,jsx,tsx}',
+    './modules/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './patterns/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
