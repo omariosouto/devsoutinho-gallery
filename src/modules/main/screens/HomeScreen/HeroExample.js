a7645322673116e396877c706a0b1716bdb2ee52
Mario Souto <mario.souto@nubank.com.br>
2021-07-10 03:12:13 -0300

    update

--- src/modules/main/screens/HomeScreen/HeroExample.js
+++ src/modules/main/screens/HomeScreen/HeroExample.js
@@ -3,18 +3,18 @@ import { MainMenu } from './patterns/MainMenu';
 
 export default function Example(props) {
   return (
-    <div className="relative bg-gray-50 overflow-hidden flex flex-col flex-1">
+    <div className="relative bg-neutral-100 overflow-hidden flex flex-col flex-1">
       <HomeBackground />
       <div className="relative pt-6 pb-16 sm:pb-24 flex flex-col flex-1">
         <MainMenu />
         <main className="mt-16 max-w-7xl px-4 sm:mt-24 container self-center grid sm:grid-cols-1 lg:grid-cols-2 gap-20 items-center content-center flex-1">
           <div className="text-left">
             {/* TODO: Ajustar a tipografia */}
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 max-w-sm sm:text-5xl md:text-6xl">
+            <h1 className="text-4xl tracking-tight font-extrabold text-neutral-900 max-w-sm sm:text-5xl md:text-6xl">
               <span className="block xl:inline">DevSoutinho </span>
-              <span className="block text-yellow-500 xl:inline">Gallery</span>
+              <span className="block text-primary-500 xl:inline">Gallery</span>
             </h1>
-            <p className="mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl max-w-sm md:max-w-md">
+            <p className="mt-3 text-base text-neutral-700 sm:text-lg md:mt-5 md:text-xl max-w-sm md:max-w-md">
               Aqui você vai encontrar diversos exemplos de projetos feitos por mim com fluxos e features comuns que você pode usar para montar seu portfólio!
             </p>
           </div>
